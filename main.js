a3f7c655a92cbbe12a38e26928973d1f0116b714
Guillem <guillemb4b@gmail.com>
2024-05-17 12:54:32 +0200

    Arrow functions 1.1

---
+++ main.js
@@ -0,0 +1,42 @@
+
+// Exercici 1
+let add = (a, b) => {return a + b};
+
+// Exercici 2
+let randomNumber = () => Math.floor(Math.random() * 101)
+
+
+// Exercici 3
+class Person {
+    constructor (name) {
+        this._name = name
+    }
+
+    get name () {
+        return this._name
+    }
+
+    greet = () => {console.log(`Hola ${this.name}`)}
+        
+}
+
+console.log(randomNumber())
+
+const persona = new Person('Ivan')
+
+console.log(persona)
+
+persona.greet()
+
+// Exercici 4 (nivell 2)
+
+const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let printNumbers = () => {for (let i = 0; i < arrayNumbers.length; i++) {  console.log(arrayNumbers[i])  } }
+
+printNumbers()
+
+// Exercici 5 (nivell 3)
+
+
+setTimeout(() => {console.log("Tengo delay de 3 segundos.");}, 3000);
